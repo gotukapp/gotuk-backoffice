b025a453eecb00956ae39a7cb3cc8663ac74120c
jcardus <joaquim_cardeira@hotmail.com>
2024-11-05 10:57:10 +0000

    firestore

--- src/routes/(app)/main/+page.svelte
+++ src/routes/(app)/main/+page.svelte
@@ -1,10 +1,18 @@
 <script>
     import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+    import { collection, getDocs } from "firebase/firestore";
+    import {onMount} from "svelte";
+    import {db} from '$lib'
+    let trips = $state([])
+    onMount(async () => {
+        const querySnapshot = await getDocs(collection(db, "trips"));
+        trips = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    });
 </script>
 <div class="w-full">
 <Table >
     <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-        Our products
+        Trips
         <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite products designed to help you work and play, stay organized, get answers, keep in touch, grow your business, and more.</p>
     </caption>
     <TableHead>
@@ -17,33 +25,18 @@
         </TableHeadCell>
     </TableHead>
     <TableBody tableBodyClass="divide-y">
-        <TableBodyRow>
-            <TableBodyCell>Apple MacBook Pro 17"</TableBodyCell>
-            <TableBodyCell>Sliver</TableBodyCell>
-            <TableBodyCell>Laptop</TableBodyCell>
-            <TableBodyCell>$2999</TableBodyCell>
-            <TableBodyCell>
-                <a href="/tables" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Edit</a>
-            </TableBodyCell>
-        </TableBodyRow>
-        <TableBodyRow>
-            <TableBodyCell>Microsoft Surface Pro</TableBodyCell>
-            <TableBodyCell>White</TableBodyCell>
-            <TableBodyCell>Laptop PC</TableBodyCell>
-            <TableBodyCell>$1999</TableBodyCell>
-            <TableBodyCell>
-                <a href="/tables" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Edit</a>
-            </TableBodyCell>
-        </TableBodyRow>
-        <TableBodyRow>
-            <TableBodyCell>Magic Mouse 2</TableBodyCell>
-            <TableBodyCell>Black</TableBodyCell>
-            <TableBodyCell>Accessories</TableBodyCell>
-            <TableBodyCell>$99</TableBodyCell>
-            <TableBodyCell>
-                <a href="/tables" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Edit</a>
-            </TableBodyCell>
-        </TableBodyRow>
+        {#each trips as trip}
+            <TableBodyRow>
+                <TableBodyCell>{trip.reservationId}</TableBodyCell>
+                <TableBodyCell>{trip.status}</TableBodyCell>
+                <TableBodyCell>{trip.date}</TableBodyCell>
+                <TableBodyCell>{trip.persons}</TableBodyCell>
+                <TableBodyCell>{trip.price}</TableBodyCell>
+                <TableBodyCell>
+                    <a href="/edit/{trips.id}" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Edit</a>
+                </TableBodyCell>
+            </TableBodyRow>
+        {/each}
     </TableBody>
 </Table>
 </div>
