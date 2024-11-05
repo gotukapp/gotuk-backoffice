0e8352ad6695b4fb66896e24327f8318642298f5
jcardus <joaquim_cardeira@hotmail.com>
2024-11-05 08:49:46 +0000

    login page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script>
 	import '../app.css';
-	let { children } = $props();
+    import {onMount} from "svelte";
+    import {goto} from "$app/navigation";
+	let { children, data } = $props();
+    onMount(async () => {
+        const user = await data.getAuthUser();
+
+        const loggedIn = !!user && user?.emailVerified;
+        if (!loggedIn) {
+            goto('/login');
+        }
+    });
+
 
 </script>
 
