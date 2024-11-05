1662e366b2912c0f255ad1faa7a213ba378e66fa
jpenas <jppenas@gmail.com>
2024-11-05 15:01:59 +0000

    trips page

--- src/routes/(app)/clients/+page.svelte
+++ src/routes/(app)/clients/+page.svelte
@@ -13,7 +13,6 @@
 <Table >
     <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
         Clients
-        <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite products designed to help you work and play, stay organized, get answers, keep in touch, grow your business, and more.</p>
     </caption>
     <TableHead>
         <TableHeadCell>Name</TableHeadCell>
