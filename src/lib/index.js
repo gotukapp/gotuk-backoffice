0043222ecc82be6f4625f984063aec36759d5939
jcardus <joaquim_cardeira@hotmail.com>
2024-11-05 08:35:07 +0000

    init firebase

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,19 @@
-// place files you want to import through the `$lib` alias in this folder.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCv0eOLm9ju0x5OnDkMfg5aP2PlpQfMMtc",
+    authDomain: "app-gotuk.firebaseapp.com",
+    projectId: "app-gotuk",
+    storageBucket: "app-gotuk.appspot.com",
+    messagingSenderId: "788543987645",
+    appId: "1:788543987645:web:121bf3115cecf4c063bcd4"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
---
+++ src/routes/+layout.js
@@ -0,0 +1,15 @@
+import {auth} from '$lib';
+import { onAuthStateChanged } from 'firebase/auth';
+
+export async function load({ url }) {
+    function getAuthUser() {
+        return new Promise((resolve) => {
+            onAuthStateChanged(auth, (user) => resolve(user ? user : false));
+        });
+    }
+
+    return {
+        getAuthUser: getAuthUser,
+        url: url.pathname
+    };
+}
